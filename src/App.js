b7dad100b9fe35b7e682242a19e227dac09c41e0
Sergey Gulaga <ry6ens@gmail.com>
2022-08-15 17:26:07 +0300

    add task-1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import AccordionItem from "./components/exercise-1/AccordionItem";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+      <li>
+        01
+        <AccordionItem />
+      </li>
+      <li>02</li>
+      <li>03</li>
+    </ul>
   );
 }
 
---
+++ src/components/exercise-1/AccordionItem.jsx
@@ -0,0 +1,53 @@
+import { Component } from "react";
+
+import styles from "./AccordionItem.module.scss";
+
+class AccordionItem extends Component {
+  state = { isActive: false };
+
+  handleClick = () => {
+    this.setState((prevState) => {
+      return { isActive: !prevState.isActive };
+    });
+  };
+
+  render() {
+    const { handleClick } = this;
+    const { isActive } = this.state;
+
+    const className = isActive
+      ? `${styles.content.isExpanded}`
+      : `${styles.content}`;
+
+    return (
+      <div className={styles.container}>
+        <div className={styles.accordion}>
+          <div className={styles.accordionItem}>
+            <p onClick={handleClick} className={styles.title}>
+              First Question
+            </p>
+            <div className={className}>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
+                eu interdum diam. Donec interdum porttitor risus non bibendum.
+                Maecenas sollicitudin eros in quam imperdiet placerat. Cras
+                justo purus, rhoncus nec lobortis ut, iaculis vel ipsum. Donec
+                dignissim arcu nec elit faucibus condimentum. Donec facilisis
+                consectetur enim sit amet varius. Pellentesque justo dui,
+                sodales quis luctus a, iaculis eget mauris. Aliquam dapibus,
+                ante quis fringilla feugiat, mauris risus condimentum massa, at
+                elementum libero quam ac ligula. Pellentesque at rhoncus dolor.
+                Duis porttitor nibh ut lobortis aliquam. Nullam eu dolor
+                venenatis mauris placerat tristique eget id dolor. Quisque
+                blandit adipiscing erat vitae dapibus. Nulla aliquam magna nec
+                elementum tincidunt.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default AccordionItem;
